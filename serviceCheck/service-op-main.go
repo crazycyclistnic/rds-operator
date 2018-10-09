be0249acb95a13b16720a0ddcb89cb0e0b1579d5
The Turkey <turkey@turkey.com>
2018-10-09 14:58:53 -0500

    adding completed pod-check and beginning of service script

---
+++ serviceCheck/service-op-main.go
@@ -0,0 +1,63 @@
+//Author: Nic Hima
+//goal of this is to create or modify services of type cluster IP
+//as of right now, only flag that works will be --deployment [deploymentName]
+//will check if service matching deployment selectors is already made
+//if not, creates the service
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	//"time"
+
+	//"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	//check to make sure commands present
+	if len(os.Args[1:]) <= 0 {
+		fmt.Fprintf(os.Stderr, "usage: %s [--deployment deploymentName]", os.Args[0])
+		os.Exit(1)
+	}
+
+	cmdArg := os.Args[1]
+
+	//check what method by which to create service from
+	switch cmdArg {
+	case "--deployment":
+		{
+			//TODO: create interface to handle different flags & thus different logic
+		}
+	case "--selector":
+		{
+			//TODO: grab next arg as comma seperated key:val pairs, and use to create service
+		}
+	default:
+		{
+			fmt.Fprintf(os.Stderr, "unknown operation: %s", cmdArg)
+			os.Exit(2)
+		}
+	}
+
+	kubeconfig := filepath.Join(
+		os.Getenv("HOME"), ".kube", "config",
+	)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//create clientset
+	c, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//TODO: pass in c as param to whatever method is being executed based on cmdArgs
+	c.AppsV1().Deployments("") //here for now so c does not throw error for not being used
+}

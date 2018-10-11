fdb6973ba127ea019c6281cf956b7e2af48c81a6
nic-hima <nhima97@gmail.com>
2018-10-11 18:54:28 -0500

    finished creating ClusterIP service based off of a deployment with --deployment flag

--- serviceCheck/service-funcs.go
+++ serviceCheck/service-funcs.go
@@ -6,11 +6,14 @@ import (
 	"log"
 	"os"
 
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	api "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-const namespace string = "default"
+const NAMESPACE string = "default"
 
 //interface used to dictate which flags func to run to create service
 type configAction interface {
@@ -28,9 +31,11 @@ type selectorMethod struct {
 //creation of service by taking selectors from a deployment
 func (conf *deploymentMethod) Execute(c *kubernetes.Clientset) {
 	fmt.Fprintf(os.Stdout, "Deployment that service is being created for is: %s\n", conf.deploymentName)
-	deploymentInterface := c.ExtensionsV1beta1().Deployments(namespace)
+	//Grab all deployments in default namespace (could be changed to select different possible namespaces)
+	deploymentInterface := c.ExtensionsV1beta1().Deployments(NAMESPACE)
+	//Check to see if deployment by name passed in as arg is present
 	deployment, err := deploymentInterface.Get(conf.deploymentName, metav1.GetOptions{})
-	if err != nil {
+	if err != nil || deployment == nil { //if deployment not present:
 		fmt.Fprintf(os.Stderr, "Could not find deployment %s\n", conf.deploymentName)
 		log.Fatal(err)
 	}
@@ -38,4 +43,30 @@ func (conf *deploymentMethod) Execute(c *kubernetes.Clientset) {
 
 	//TODO: using the found deployment, create a service of type clusterIP that matches the selectors in
 	//the deployment
+
+	//Define service spec based on deployment
+	serviceSpec := &api.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      deployment.Name + "-service",
+			Namespace: deployment.Namespace,
+		},
+		Spec: api.ServiceSpec{
+			Type:     api.ServiceTypeClusterIP,
+			Selector: deployment.Spec.Selector.MatchLabels,
+			Ports: []api.ServicePort{
+				api.ServicePort{
+					Port:       80,
+					TargetPort: intstr.FromInt(80),
+				},
+			},
+		},
+	}
+
+	fmt.Fprintf(os.Stdout, "Creating service named: %s...\n", serviceSpec.GetName())
+	//Create service
+	_, err = c.CoreV1().Services(deployment.Namespace).Create(serviceSpec)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
